a4a96ad811f3a96787b347ad28c53124b54e9632
yasshexe <yashmane.448@gmail.com>
2026-08-23 20:56:47 +0530

    Add About page route

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home
+from .views import about, home
 
 urlpatterns = [
     path("", home, name="home"),
+    path("about/", about, name="about"),
 ]
